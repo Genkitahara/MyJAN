c5af2c6f91ca5718c5c7b9e827fdee5def303f62
Genkitahara <gen19990401@gmail.com>
2020-01-21 01:30:34 +0900

    testyanen

--- app/src/main/java/com/example/myjan/MainActivity.kt
+++ app/src/main/java/com/example/myjan/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val constraintLayout = findViewById(R.id.constraintLayout) as ConstraintLayout
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
